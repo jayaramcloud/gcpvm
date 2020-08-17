0c217f8fe8b96cd08ebacf4f5f01ff3d13e84972
jayaramcloud <jayaram.linux@gmail.com>
2020-08-16 23:46:36 -0600

    first commit

---
+++ modules/uc1/variable.tf
@@ -0,0 +1,15 @@
+variable "var_project" {
+        default = "norse-baton-285620"
+    }
+variable "var_env" {
+        default = "dev"
+    }
+variable "var_company" { 
+        default = "company-name"
+    }
+variable "uc1_private_subnet" {
+        default = "10.26.1.0/24"
+    }
+variable "uc1_public_subnet" {
+        default = "10.26.2.0/24"
+    }
